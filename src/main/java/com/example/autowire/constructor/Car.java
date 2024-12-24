cb3a87aa04faa8b2d02ab21ee82f1162445e1a53
GarimaUttam <garimauttam191203@gmail.com>
2024-12-25 00:33:08 +0530

    autowiring BY Type

---
+++ src/main/java/com/example/autowire/constructor/Car.java
@@ -0,0 +1,13 @@
+package com.example.autowire.constructor;
+
+public class Car {
+    private Specification specification1;
+
+    public void setSpecification1(Specification specification) {
+        this.specification1 = specification;
+    }
+
+    public void displayDetails(){
+        System.out.println("Car Details: " + specification1.toString());
+    }
+}
