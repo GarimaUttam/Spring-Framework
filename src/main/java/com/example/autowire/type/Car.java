3fa0b5e934f8a65e18caa1d42c92b3f2650d77f7
GarimaUttam <garimauttam191203@gmail.com>
2024-12-25 00:13:50 +0530

    autowiring by Type done

--- src/main/java/com/example/autowire/type/Car.java
+++ src/main/java/com/example/autowire/type/Car.java
@@ -1,16 +1,6 @@
 package com.example.autowire.type;
 
 public class Car {
-<<<<<<< HEAD
-    private Specification specification;
-
-    public void setSpecification(Specification specification) {
-        this.specification = specification;
-    }
-
-    public void displayDetails(){
-        System.out.println("Car Details: " + specification.toString());
-=======
     private Specification specification1;
 
     public void setSpecification1(Specification specification) {
@@ -19,6 +9,5 @@ public class Car {
 
     public void displayDetails(){
         System.out.println("Car Details: " + specification1.toString());
->>>>>>> 6580bd9 (autowiring by type)
     }
 }
