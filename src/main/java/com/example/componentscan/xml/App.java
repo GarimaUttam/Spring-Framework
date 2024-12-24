89d120315e11a5b32352bf4fb7acc5d502c9fc78
GarimaUttam <garimauttam191203@gmail.com>
2024-12-25 02:45:24 +0530

    component scam using XML file

--- src/main/java/com/example/componentscan/xml/App.java
+++ src/main/java/com/example/componentscan/xml/App.java
@@ -1,4 +1,4 @@
-package com.example.componentscan;
+package com.example.componentscan.xml;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
--- src/main/java/com/example/componentscan/xml/Employee.java
+++ src/main/java/com/example/componentscan/xml/Employee.java
@@ -1,4 +1,4 @@
-package com.example.componentscan;
+package com.example.componentscan.xml;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
