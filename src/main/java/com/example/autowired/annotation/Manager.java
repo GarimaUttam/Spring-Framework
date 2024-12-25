04ff696d6bec8d41d2fc6c7378c345cb7cdf2b7e
GarimaUttam <garimauttam191203@gmail.com>
2024-12-26 01:43:18 +0530

    spring framework done

--- src/main/java/com/example/autowired/annotation/Manager.java
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,11 +1,14 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
     @Autowired
+//    bean of same type but different names
+    @Qualifier("employee1")
     private Employee employee;
 
 //    @Autowired
